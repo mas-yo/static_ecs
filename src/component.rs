e8773b7c946c300164f98a39dcf2b954aa8f0af6
Masahiro Yoshida <mypianoplayer@gmail.com>
2021-02-10 01:51:54 +0000

    make EntityID to trait ID

--- src/component.rs
+++ src/component.rs
@@ -1,32 +1,49 @@
 use std::collections::HashMap;
+use std::hash::Hash;
 
-#[derive(Clone, Copy, Eq, PartialEq, Hash, Default)]
-pub struct EntityID {
-    id: u32,
+trait ID {
+    fn next(&self) -> Self;
 }
-impl EntityID {
-    pub fn new() -> Self {
-        Self { id: 0 }
-    }
-    pub fn next(&self) -> Self {
-        Self { id: self.id + 1 }
+
+impl ID for u32 {
+    fn next(&self) -> Self {
+        self + 1
     }
 }
 
-pub struct Component<T> {
-    entity_id: EntityID,
+// #[derive(Clone, Copy, Eq, PartialEq, Hash, Default)]
+// pub struct EntityID {
+//     id: u32,
+// }
+// impl EntityID {
+//     pub fn new() -> Self {
+//         Self { id: 0 }
+//     }
+//     pub fn next(&self) -> Self {
+//         Self { id: self.id + 1 }
+//     }
+// }
+
+pub struct Component<I, T> {
+    entity_id: I,
     inner: T,
 }
 
-impl<T> Component<T> {
-    pub fn new(entity_id: EntityID, inner: T) -> Self {
+impl<I, T> Component<I, T>
+where
+    I: Copy,
+{
+    pub fn entity_id(&self) -> I {
+        self.entity_id
+    }
+}
+
+impl<I, T> Component<I, T> {
+    pub fn new(entity_id: I, inner: T) -> Self {
         Self {
             entity_id: entity_id,
             inner: inner,
         }
-    }
-    pub fn entity_id(&self) -> EntityID {
-        self.entity_id
     }
     pub fn inner(&self) -> &T {
         &self.inner
@@ -36,15 +53,18 @@ impl<T> Component<T> {
     }
 }
 
-pub type CContainer<T> = ComponentContainer<T>;
+pub type CContainer<I, T> = ComponentContainer<I, T>;
 
 // #[derive(Default)]
-pub struct ComponentContainer<T> {
-    map: HashMap<EntityID, usize>,
-    vec: Vec<Component<T>>,
+pub struct ComponentContainer<I, T> {
+    map: HashMap<I, usize>,
+    vec: Vec<Component<I, T>>,
 }
 
-impl<T> Default for ComponentContainer<T> {
+impl<I, T> Default for ComponentContainer<I, T>
+where
+    I: Eq + Hash,
+{
     fn default() -> Self {
         Self {
             map: HashMap::new(),
@@ -53,46 +73,52 @@ impl<T> Default for ComponentContainer<T> {
     }
 }
 
-impl<T> ComponentContainer<T> {
-    pub fn push(&mut self, entity_id: EntityID, item: T) {
-        self.vec.push(Component::<T>::new(entity_id, item));
+impl<I, T> ComponentContainer<I, T>
+where
+    I: Copy + Eq + Hash,
+{
+    pub fn push(&mut self, entity_id: I, item: T) {
+        self.vec.push(Component::<I, T>::new(entity_id, item));
         self.map.insert(entity_id, self.vec.len() - 1);
     }
-    pub fn get(&self, entity_id: EntityID) -> Option<&T> {
+    pub fn get(&self, entity_id: I) -> Option<&T> {
         let index = self.map.get(&entity_id)?;
         Some(self.vec[*index].inner())
     }
-    pub fn get_mut(&mut self, entity_id: EntityID) -> Option<&mut T> {
+    pub fn get_mut(&mut self, entity_id: I) -> Option<&mut T> {
         let index = self.map.get(&entity_id)?;
         Some(self.vec[*index].inner_mut())
     }
-    pub fn iter(&self) -> ComponentIter<T> {
+    pub fn iter(&self) -> ComponentIter<I, T> {
         ComponentIter {
             iter: self.vec.iter(),
         }
     }
-    pub fn iter_mut(&mut self) -> ComponentIterMut<T> {
+    pub fn iter_mut(&mut self) -> ComponentIterMut<I, T> {
         ComponentIterMut {
             iter: self.vec.iter_mut(),
         }
     }
 }
 
-pub struct ComponentIter<'a, T>
+pub struct ComponentIter<'a, I, T>
 where
     T: 'a,
 {
-    iter: std::slice::Iter<'a, Component<T>>,
+    iter: std::slice::Iter<'a, Component<I, T>>,
 }
-impl<'a, T> Iterator for ComponentIter<'a, T> {
-    type Item = (EntityID, &'a T);
+impl<'a, I, T> Iterator for ComponentIter<'a, I, T>
+where
+    I: Copy,
+{
+    type Item = (I, &'a T);
     fn next(&mut self) -> Option<Self::Item> {
         let next = self.iter.next()?;
         Some((next.entity_id(), next.inner()))
     }
 }
-impl<'a, T> ComponentIter<'a, T> {
-    pub fn zip_entity<U>(self, other: &'a CContainer<U>) -> ZipEntity<'a, T, U> {
+impl<'a, I, T> ComponentIter<'a, I, T> {
+    pub fn zip_entity<U>(self, other: &'a CContainer<I, U>) -> ZipEntity<'a, I, T, U> {
         ZipEntity {
             base: self,
             other: other,
@@ -100,9 +126,9 @@ impl<'a, T> ComponentIter<'a, T> {
     }
     pub fn zip_entity2<U, V>(
         self,
-        other1: &'a CContainer<U>,
-        other2: &'a CContainer<V>,
-    ) -> ZipEntity2<'a, T, U, V> {
+        other1: &'a CContainer<I, U>,
+        other2: &'a CContainer<I, V>,
+    ) -> ZipEntity2<'a, I, T, U, V> {
         ZipEntity2 {
             base: self,
             other1: other1,
@@ -110,21 +136,24 @@ impl<'a, T> ComponentIter<'a, T> {
         }
     }
 }
-pub struct ComponentIterMut<'a, T>
+pub struct ComponentIterMut<'a, I, T>
 where
     T: 'a,
 {
-    iter: std::slice::IterMut<'a, Component<T>>,
+    iter: std::slice::IterMut<'a, Component<I, T>>,
 }
-impl<'a, T> Iterator for ComponentIterMut<'a, T> {
-    type Item = (EntityID, &'a mut T);
+impl<'a, I, T> Iterator for ComponentIterMut<'a, I, T>
+where
+    I: Copy,
+{
+    type Item = (I, &'a mut T);
     fn next(&mut self) -> Option<Self::Item> {
         let next = self.iter.next()?;
         Some((next.entity_id(), next.inner_mut()))
     }
 }
-impl<'a, T> ComponentIterMut<'a, T> {
-    pub fn zip_entity<U>(self, other: &'a CContainer<U>) -> ZipEntityMut<'a, T, U> {
+impl<'a, I, T> ComponentIterMut<'a, I, T> {
+    pub fn zip_entity<U>(self, other: &'a CContainer<I, U>) -> ZipEntityMut<'a, I, T, U> {
         ZipEntityMut {
             base: self,
             other: other,
@@ -132,9 +161,9 @@ impl<'a, T> ComponentIterMut<'a, T> {
     }
     pub fn zip_entity2<U, V>(
         self,
-        other1: &'a CContainer<U>,
-        other2: &'a CContainer<V>,
-    ) -> ZipEntity2Mut<'a, T, U, V> {
+        other1: &'a CContainer<I, U>,
+        other2: &'a CContainer<I, V>,
+    ) -> ZipEntity2Mut<'a, I, T, U, V> {
         ZipEntity2Mut {
             base: self,
             other1: other1,
@@ -144,10 +173,10 @@ impl<'a, T> ComponentIterMut<'a, T> {
 
     pub fn zip_entity3<U, V, W>(
         self,
-        other1: &'a CContainer<U>,
-        other2: &'a CContainer<V>,
-        other3: &'a CContainer<W>,
-    ) -> ZipEntity3Mut<'a, T, U, V, W> {
+        other1: &'a CContainer<I, U>,
+        other2: &'a CContainer<I, V>,
+        other3: &'a CContainer<I, W>,
+    ) -> ZipEntity3Mut<'a, I, T, U, V, W> {
         ZipEntity3Mut {
             base: self,
             other1: other1,
@@ -157,17 +186,20 @@ impl<'a, T> ComponentIterMut<'a, T> {
     }
 }
 
-pub struct ZipEntity<'a, T, U>
+pub struct ZipEntity<'a, I, T, U>
 where
     T: 'a,
     U: 'a,
 {
-    base: ComponentIter<'a, T>,
-    other: &'a CContainer<U>,
+    base: ComponentIter<'a, I, T>,
+    other: &'a CContainer<I, U>,
 }
 
-impl<'a, T, U> Iterator for ZipEntity<'a, T, U> {
-    type Item = (EntityID, &'a T, &'a U);
+impl<'a, I, T, U> Iterator for ZipEntity<'a, I, T, U>
+where
+    I: Copy + Eq + Hash,
+{
+    type Item = (I, &'a T, &'a U);
     fn next(&mut self) -> Option<Self::Item> {
         while let Some((entity_id, base)) = self.base.next() {
             if let Some(other_item) = self.other.get(entity_id) {
@@ -178,17 +210,20 @@ impl<'a, T, U> Iterator for ZipEntity<'a, T, U> {
     }
 }
 
-pub struct ZipEntityMut<'a, T, U>
+pub struct ZipEntityMut<'a, I, T, U>
 where
     T: 'a,
     U: 'a,
 {
-    base: ComponentIterMut<'a, T>,
-    other: &'a CContainer<U>,
+    base: ComponentIterMut<'a, I, T>,
+    other: &'a CContainer<I, U>,
 }
 
-impl<'a, T, U> Iterator for ZipEntityMut<'a, T, U> {
-    type Item = (EntityID, &'a mut T, &'a U);
+impl<'a, I, T, U> Iterator for ZipEntityMut<'a, I, T, U>
+where
+    I: Copy + Eq + Hash,
+{
+    type Item = (I, &'a mut T, &'a U);
     fn next(&mut self) -> Option<Self::Item> {
         while let Some((entity_id, base)) = self.base.next() {
             if let Some(other_item) = self.other.get(entity_id) {
@@ -198,19 +233,22 @@ impl<'a, T, U> Iterator for ZipEntityMut<'a, T, U> {
         None
     }
 }
-pub struct ZipEntity2<'a, T, U, V>
+pub struct ZipEntity2<'a, I, T, U, V>
 where
     T: 'a,
     U: 'a,
     V: 'a,
 {
-    base: ComponentIter<'a, T>,
-    other1: &'a CContainer<U>,
-    other2: &'a CContainer<V>,
+    base: ComponentIter<'a, I, T>,
+    other1: &'a CContainer<I, U>,
+    other2: &'a CContainer<I, V>,
 }
 
-impl<'a, T, U, V> Iterator for ZipEntity2<'a, T, U, V> {
-    type Item = (EntityID, &'a T, &'a U, &'a V);
+impl<'a, I, T, U, V> Iterator for ZipEntity2<'a, I, T, U, V>
+where
+    I: Copy + Eq + Hash,
+{
+    type Item = (I, &'a T, &'a U, &'a V);
     fn next(&mut self) -> Option<Self::Item> {
         while let Some((entity_id, base)) = self.base.next() {
             let other1_item = self.other1.get(entity_id);
@@ -223,19 +261,22 @@ impl<'a, T, U, V> Iterator for ZipEntity2<'a, T, U, V> {
     }
 }
 
-pub struct ZipEntity2Mut<'a, T, U, V>
+pub struct ZipEntity2Mut<'a, I, T, U, V>
 where
     T: 'a,
     U: 'a,
     V: 'a,
 {
-    base: ComponentIterMut<'a, T>,
-    other1: &'a CContainer<U>,
-    other2: &'a CContainer<V>,
+    base: ComponentIterMut<'a, I, T>,
+    other1: &'a CContainer<I, U>,
+    other2: &'a CContainer<I, V>,
 }
 
-impl<'a, T, U, V> Iterator for ZipEntity2Mut<'a, T, U, V> {
-    type Item = (EntityID, &'a mut T, &'a U, &'a V);
+impl<'a, I, T, U, V> Iterator for ZipEntity2Mut<'a, I, T, U, V>
+where
+    I: Copy + Eq + Hash,
+{
+    type Item = (I, &'a mut T, &'a U, &'a V);
     fn next(&mut self) -> Option<Self::Item> {
         while let Some((entity_id, base)) = self.base.next() {
             let other1_item = self.other1.get(entity_id);
@@ -248,20 +289,23 @@ impl<'a, T, U, V> Iterator for ZipEntity2Mut<'a, T, U, V> {
     }
 }
 
-pub struct ZipEntity3Mut<'a, T, U, V, W>
+pub struct ZipEntity3Mut<'a, I, T, U, V, W>
 where
     T: 'a,
     U: 'a,
     V: 'a,
     W: 'a,
 {
-    base: ComponentIterMut<'a, T>,
-    other1: &'a CContainer<U>,
-    other2: &'a CContainer<V>,
-    other3: &'a CContainer<W>,
+    base: ComponentIterMut<'a, I, T>,
+    other1: &'a CContainer<I, U>,
+    other2: &'a CContainer<I, V>,
+    other3: &'a CContainer<I, W>,
 }
 
-impl<'a, T, U, V, W> Iterator for ZipEntity3Mut<'a, T, U, V, W> {
+impl<'a, I, T, U, V, W> Iterator for ZipEntity3Mut<'a, I, T, U, V, W>
+where
+    I: Copy + Eq + Hash,
+{
     type Item = (&'a mut T, &'a U, &'a V, &'a W);
     fn next(&mut self) -> Option<Self::Item> {
         while let Some((entity_id, base)) = self.base.next() {
