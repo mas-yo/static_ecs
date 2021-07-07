9ee3158662c6a5f7c8b9fd9ef9b42b3a18dd251d
Masahiro Yoshida <mypianoplayer@gmail.com>
2021-07-07 19:47:18 +0000

    get_component

--- static_ecs/src/lib.rs
+++ static_ecs/src/lib.rs
@@ -24,10 +24,42 @@ pub trait RemoveComponent {
     fn remove_component(&mut self, entity_id: EntityID);
 }
 
+pub trait GetComponent<'a, R>
+where
+    R: 'a,
+{
+    fn get_component(&'a self) -> R;
+}
+pub trait GetComponentMut<'a, R>
+where
+    R: 'a,
+{
+    fn get_component_mut(&'a mut self) -> R;
+}
+
 #[derive(Default)]
 struct World<CC> {
     next_entity_id: EntityID,
     components: CC,
+}
+
+impl<'a, R, CC> GetComponent<'a, R> for World<CC>
+where
+    R: 'a,
+    CC: TypeRef<'a, R>,
+{
+    fn get_component(&'a self) -> R {
+        self.components.type_ref()
+    }
+}
+impl<'a, R, CC> GetComponentMut<'a, R> for World<CC>
+where
+    R: 'a,
+    CC: TypeRefMut<'a, R>,
+{
+    fn get_component_mut(&'a mut self) -> R {
+        self.components.type_ref_mut()
+    }
 }
 // impl<CC, C> PushComponent<C> for World<CC>
 // where
@@ -73,20 +105,30 @@ macro_rules! add_entity {
     };
 }
 
-pub fn system0<T, P>(t: &mut CContainer<T>, pred: P)
+pub fn system0<T, P>(container: &mut CContainer<T>, pred: P)
 where
     P: Fn(EntityID, &mut T),
 {
-    for (eid, d) in t.iter_mut() {
+    for (eid, d) in container.iter_mut() {
         pred(eid, d);
     }
 }
-pub fn system1<T1, T2, P>(tpl: (&mut CContainer<T1>, &CContainer<T2>), pred: P)
+pub fn system1<T1, T2, P>(container: (&mut CContainer<T1>, &CContainer<T2>), pred: P)
 where
     P: Fn(EntityID, &mut T1, &T2),
 {
-    for (eid, d1, d2) in tpl.0.iter_mut().zip_entity(tpl.1) {
+    for (eid, d1, d2) in container.0.iter_mut().zip_entity(container.1) {
         pred(eid, d1, d2);
+    }
+}
+pub fn system2<T1, T2, T3, P>(
+    container: (&mut CContainer<T1>, &CContainer<T2>, &CContainer<T3>),
+    pred: P,
+) where
+    P: Fn(EntityID, &mut T1, &T2, &T3),
+{
+    for (eid, d1, d2, d3) in container.0.iter_mut().zip_entity2(container.1, container.2) {
+        pred(eid, d1, d2, d3);
     }
 }
 
@@ -97,6 +139,8 @@ mod tests {
     use crate::component::*;
     use crate::system1;
     use crate::CContainer;
+    use crate::GetComponent;
+    use crate::GetComponentMut;
     use crate::PushComponent;
     use crate::TypeRef;
     use crate::TypeRefMut;
@@ -112,11 +156,11 @@ mod tests {
         // let (c1, c2) = w.components.type_ref_mut();
 
         system1(
-            w.components.type_ref_mut(),
+            w.get_component_mut(), // components.type_ref_mut(),
             |entity_id, d1: &mut i32, d2: &u32| *d1 += *d2 as i32,
         );
 
-        let ci32: (&CContainer<i32>) = w.components.type_ref();
+        let ci32: (&CContainer<i32>) = w.get_component();
         for i in ci32.iter() {
             println!("{}", i.1);
         }
